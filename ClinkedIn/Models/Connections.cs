642c7fc9f4884d403d924cd1ba8f5a7233e16669
Shane D Wilson <me@shanedwilson.com>
2019-04-09 20:26:59 -0500

    model done

--- ClinkedIn/Models/Connections.cs
+++ ClinkedIn/Models/Connections.cs
@@ -5,7 +5,18 @@ using System.Threading.Tasks;
 
 namespace ClinkedIn.Models
 {
-    public class Connections
+    public class Connection
     {
+        public int Id;
+        public int UserId1;
+        public int UserId2;
+        public bool IsFriend;
+
+        public Connection(int userId1, int userId2, bool isFriend)
+        {
+            UserId1 = userId1;
+            UserId2 = userId2;
+            IsFriend = isFriend;
+        }
     }
 }
