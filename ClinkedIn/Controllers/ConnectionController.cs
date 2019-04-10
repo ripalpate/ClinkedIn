cdfd98d2795758cca715674a6a3d09c61da1fda8
Shane D Wilson <me@shanedwilson.com>
2019-04-09 20:57:55 -0500

    controller and repository done

---
+++ ClinkedIn/Controllers/ConnectionController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ClinkedIn.Data;
+using ClinkedIn.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClinkedIn.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ConnectionController : ControllerBase
+    {
+        readonly ConnectionRepository _connectionRepository;
+
+       public ConnectionController()
+        {
+            _connectionRepository = new ConnectionRepository();
+        }
+
+        [HttpPost("connect")]
+
+        public ActionResult AddConnection(Connection connection)
+        {
+            var newConnection = _connectionRepository.AddConnection(connection.UserId1, connection.UserId2, connection.IsFriend);
+
+            return Created($"api/connect/{newConnection.Id}", newConnection);
+        }
+    }
+}
---
+++ ClinkedIn/Data/ConnectionRepository.cs
@@ -0,0 +1,24 @@
+﻿using ClinkedIn.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClinkedIn.Data
+{
+    public class ConnectionRepository
+    {
+        static List<Connection> _connections = new List<Connection>();
+
+        public Connection AddConnection(int userId1, int userId2, bool isFriend)
+        {
+            var newConnection = new Connection(userId1, userId2, isFriend);
+
+            newConnection.Id = _connections.Count + 1;
+
+            _connections.Add(newConnection);
+
+            return newConnection;
+        }
+    }
+}
