7d6179ab880ebd88e11e491a626432e82737869c
Ripal Patel <ripal212007@gmail.com>
2019-04-09 23:19:53 -0500

    interest are adding

---
+++ ClinkedIn/Controllers/InterestsController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ClinkedIn.Data;
+using ClinkedIn.Models;
+using ClinkedIn.Validators;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClinkedIn.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InterestsController : ControllerBase
+    {
+        readonly InterestRepository _interestRepository;
+        readonly CreateInterestValidator _validator;
+        public InterestsController()
+        {
+            _validator = new CreateInterestValidator();
+            _interestRepository = new InterestRepository();
+
+        }
+
+        [HttpPost]
+
+        public ActionResult AddInterest(CreateInterestRequest createRequest)
+        {
+            if (!_validator.ValidateInterest(createRequest))
+            {
+                return BadRequest(new { error = "users must have an interest name" });
+            }
+
+            var newInterestList = _interestRepository.AddInterest(createRequest.InterestName, createRequest.UserId);
+            var listOfSimilarUserId = newInterestList.Where(x => x.UserId == createRequest.UserId).ToList();
+            return Created($"api/{listOfSimilarUserId}", listOfSimilarUserId);
+        }
+    }
+}
---
+++ ClinkedIn/Data/InterestRepository.cs
@@ -0,0 +1,23 @@
+﻿using ClinkedIn.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClinkedIn.Data
+{
+    public class InterestRepository
+    {
+        static List<Interest> _interests = new List<Interest>();
+        public List<Interest> AddInterest(string interestName, int userId)
+        {
+            var newInterest = new Interest(interestName, userId);
+
+            newInterest.Id = _interests.Count + 1;
+
+            _interests.Add(newInterest);
+
+            return _interests;
+        }
+    }
+}
