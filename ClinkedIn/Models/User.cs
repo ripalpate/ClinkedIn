f92ee9dfd2b34ba393a06cfdd96572248210af2c
Ripal Patel <ripal212007@gmail.com>
2019-04-09 19:14:55 -0500

    added user model

--- ClinkedIn/Models/User.cs
+++ ClinkedIn/Models/User.cs
@@ -7,5 +7,22 @@ namespace ClinkedIn.Models
 {
     public class User
     {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string DisplayName { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public int Wallet { get; set; }
+        public string Offense { get; set; }
+
+        public User(string username, string password, string displayName, DateTime releaseDate, int wallet, string offense)
+            {
+                Username = username;
+                Password = password;
+                DisplayName = displayName;
+                ReleaseDate = releaseDate;
+                Wallet = wallet;
+                Offense = offense;
+            }
     }
 }
