dbeb8e4d0c0a786b30a068fa0817213c3ee5f44a
Shane D Wilson <me@shanedwilson.com>
2019-04-09 19:30:52 -0500

    ready

--- ClinkedIn/Data/UserRepository.cs
+++ ClinkedIn/Data/UserRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ClinkedIn.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,9 +10,9 @@ namespace ClinkedIn.Data
     {
         static List<User> _users = new List<User>();
 
-        public User AddUser(string username, string password)
+        public User AddUser(string username, string password, string displayName)
         {
-            var newUser = new User(username, password);
+            var newUser = new User(username, password, displayName);
 
             newUser.Id = _users.Count + 1;
 
