8bf10dd950c3ea12fe97cd26fed4ceba5c63f992
Robert Rice <rrice1291@gmail.com>
2019-04-13 09:49:13 -0500

    wip

--- ClinkedIn/Models/UserService.cs
+++ ClinkedIn/Models/UserService.cs
@@ -7,11 +7,6 @@ namespace ClinkedIn.Models
 {
     public class UserService
     {
-        public int Id { get; set; }
-        public int UserId { get; set; }
-        public int ServiceId { get; set; }
-
-        public UserService(int id, int userid, int serviceid)
         {
             Id = id;
             UserId = userid;
