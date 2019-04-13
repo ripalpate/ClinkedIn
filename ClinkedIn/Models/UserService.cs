75d600e6838abf9b2d2677c24fd6ffb5c92c388a
Robert Rice <rrice1291@gmail.com>
2019-04-13 09:53:30 -0500

    added updateServiceRequest

--- ClinkedIn/Models/UserService.cs
+++ ClinkedIn/Models/UserService.cs
@@ -7,6 +7,11 @@ namespace ClinkedIn.Models
 {
     public class UserService
     {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int ServiceId { get; set; }
+
+        public UserService(int id, int userid, int serviceid)
         {
             Id = id;
             UserId = userid;
